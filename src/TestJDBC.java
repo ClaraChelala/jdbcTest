7d664d7c02517116e2f89c1a05d3576facd3caa4
ClaraChelala <chelalaclara@gmail.com>
2022-01-31 00:00:13 +0200

    fixes

--- src/TestJDBC.java
+++ src/TestJDBC.java
@@ -1,19 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
 public class TestJDBC {
 
     public static void main( String[] args ) throws Exception {
-        Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+        Class.forName("org.postgresql.Drivermanager");
         Connection cx = DriverManager.getConnection(
-                "jdbc:derby:mabase"
+                "jdbc:postgresql://localhost:5432/postgres"
                 ,
-                "login"
+                "postgres"
                 ,
-                "passwd");
+                "12345");
         Statement st = cx.createStatement();
-        ResultSet rs = st.executeQuery( "SELECT * FROM personnes" );
+        ResultSet rs = st.executeQuery( "SELECT * FROM students" );
         while (rs.next()) {
-            String nom = rs.getString("nom");
+            String firstname = rs.getString("firstname");
+            String lastname = rs.getString("lastname");
             int age = rs.getInt("age");
-            System.out.println( nom + " a " + age + " ans" );
+            System.out.println( firstname + lastname + " a " + age + " ans" );
         }
         rs.close();
         st.close();
